e876b52dbcf2a9dd62e5b226b2ed99949502ba89
PENG AO <pengao1293@gmail.com>
2022-03-19 22:07:24 +0900

    asm-data-structure: 2d array operation

--- asm/test-codes/1d-list-test.s
+++ asm/test-codes/1d-list-test.s
@@ -2,10 +2,11 @@
 
     .text
 main:
-    # create an 5words-array
+    # create a 5words-array
     li a0, 5
     call malloc
-    mv s1, a0 # s0 = u32[5]
+    mv s1, a0 # s1 = u32[5]
+
     # init every item with its index
     li t0, 0
     li t1, 5
@@ -18,21 +19,24 @@ init_loop_start:
     addi t0, t0, 1
     j init_loop_start
 init_loop_end:
-    ebreak
+
     # print out each item
     li t0, 0
     li t1, 5
 print_loop_start:
     bge t0, t1, print_loop_end
+    # access item
     mv a0, s1
     mv a1, t0
-    call get_nth # a0 = s0[t0]
+    call get_nth
+    # print int
     mv a1, a0
     li a0, 1
-    ecall # print_int
+    ecall
     addi t0, t0, 1
     j print_loop_start
 print_loop_end:
+
     # exit
     li a0, 0
     ecall
