4ed8aa7d1bd7ed35b07e7ea03de59a59563915fa
PENG AO <pengao1293@gmail.com>
2022-03-21 17:55:53 +0900

    asm-data-structure: update the test file of slist

--- asm/test-codes/slist-test.s
+++ asm/test-codes/slist-test.s
@@ -6,7 +6,6 @@ main:
     li a0, 5
     call make_slist
     mv s0, a0 # s0 = slist
-    ebreak
 
     # append 1 ~ 5
     li s1, 1
@@ -19,7 +18,6 @@ append_loop_start:
     addi s1, s1, 1
     j append_loop_start
 append_loop_end:
-    ebreak
 
     # is empty?
     mv a0, s0 # a0 = slist
@@ -27,55 +25,46 @@ append_loop_end:
     mv a1, a0
     li a0, 2
     ecall # print bool
-    ebreak
 
     # filter
     mv a0, s0 # a0 = slist
     la a1, is_odd
     call slist_filter # a0 = filtered slist
-    ebreak
+
     mv s1, a0
     li a1, 0
     li a2, 2
     call slist_insert
-    ebreak
+
     mv a0, s1
     lw a1, 0(a0)
     li a2, 4
     call slist_insert
-    ebreak
+
     mv a0, s1
     la a1, print_item
     li a2, 1
     call slist_map
-    ebreak
 
-    # map
-    mv a0, s0 # a0 = slist
-    la a1, calc_double
-    li a2, 1 # a2 = in place
-    call slist_map # a0 = mapped slist
-    ebreak
-
-    # remove 3
+    # find 3
     mv a0, s0 # a0 = slist
     li a1, 3
     call slist_find # a0 = index
-    ebreak
+
+    # remove idx of 3
     mv a1, a0 # a1 = index
     mv a0, s0 # a0 = slist
     call slist_remove
-    ebreak
 
-    # remove 6
+    # find 6
     mv a0, s0 # a0 = slist
     li a1, 6
     call slist_find # a0 = index
-    ebreak
+
+    # remove idx of 6
     mv a1, a0 # a1 = index
     mv a0, s0 # a0 = slist
     call slist_remove
-    ebreak
 
     # pop until empty
 pop_loop_start:
@@ -89,14 +78,9 @@ pop_loop_start:
     ecall # print int
     j pop_loop_start
 pop_loop_end:
-    ebreak
 
     li a0, 0
     ecall
-
-calc_double:
-    slli a0, a0, 1
-    ret
 
 is_odd:
     andi a0, a0, 1
